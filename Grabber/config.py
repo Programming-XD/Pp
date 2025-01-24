1430806e649a184b76a0ed79e7f85124a6f92de7
ᴀᴋɪᴛᴏ <fr.programmer.xd@gmail.com>
2025-01-24 12:51:37 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -1,15 +1,15 @@
 import os
 
 OWNER_ID = 7455169019
-GROUP_ID = "-1002225496870"
-TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = "-1002383819050"
+TOKEN = os.getenv("7256217879:AAFsip_4QFFaSLxLKz5fmW6zPXYTRlB4xgw")
 api_id = 20457610
 api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
 PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
-SUPPORT_CHAT = "dragona_support"
-UPDATE_CHAT = "PickBotUpdatesHQ"
-BOT_USERNAME = "Okarun_Game_bot"
-CHARA_CHANNEL_ID = -1002235251549
+SUPPORT_CHAT = "BeybladeVerse"
+UPDATE_CHAT = "BeybladeUpdate"
+BOT_USERNAME = "YxH_Game_bot"
+CHARA_CHANNEL_ID = -1002383819050
 MONGO_URL = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
 DATABASE_NAME = "Character_catcher"
-LOG_CHAT_ID = -1002203193964
+LOG_CHAT_ID = -1002383819050
